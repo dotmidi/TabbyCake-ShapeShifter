85265cbd253e830c00d9ca0249faf339d7f86ef8
DreeseR <roandries@hotmail.com>
2024-09-06 16:01:34 +0200

    First simple beginning of the game made

--- My project/Assets/SidescrollScript.cs
+++ My project/Assets/SidescrollScript.cs
@@ -7,6 +7,11 @@ public class SidescrollScript : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
 
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject player;
+    private PlayerScript playerScript;
+
     //[Header("Spawning")]
     //private TopAndBottomSpawn spawner;
     //private bool doOnce = true;
@@ -15,13 +20,25 @@ public class SidescrollScript : MonoBehaviour
     void Start()
     {
         //spawner = GameObject.FindGameObjectWithTag("TopAndBottomSpawner").GetComponent<TopAndBottomSpawn>();
+        playerScript = player.GetComponent<PlayerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if (playerScript.alive)
+        {
+            rb.velocity = Vector2.left * moveSpeed;
+        }
+        else if (playerScript.alive == false)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+
         /*
+        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        
         if (transform.position.x < 8 && doOnce)
         {
             spawner.xPosPrev = transform.position.x;
