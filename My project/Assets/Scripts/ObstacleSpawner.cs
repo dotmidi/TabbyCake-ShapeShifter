842cc9f2e642e4b43287211ae8b13b184a4fca20
dotmidi <0994510@hr.nl>
2024-09-09 15:23:15 +0200

    spawning obstacles, scaling with device

---
+++ My project/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public List<GameObject> obstacles;
+    public int intervalBetweenObstacles;
+    public int obstacleSpeedMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObstacles());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnObstacles()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(intervalBetweenObstacles);
+            int randomIndex = Random.Range(0, obstacles.Count);
+            float randomY = Random.value > 0.5f ? 4f : -4f;  // Randomly choose either -4 or 4
+            // if randomY is 4, flip the obstacle upside down
+            if (randomY > 0)
+            {
+                GameObject obstacle = Instantiate(obstacles[randomIndex], new Vector3(10, randomY, 0), Quaternion.Euler(0, 0, 180));
+                Rigidbody2D obstacleRigidbody = obstacle.GetComponent<Rigidbody2D>();
+                obstacleRigidbody.velocity = Vector2.left * obstacleSpeedMultiplier;
+            }
+            else
+            {
+                GameObject obstacle = Instantiate(obstacles[randomIndex], new Vector3(10, randomY, 0), Quaternion.identity);
+                Rigidbody2D obstacleRigidbody = obstacle.GetComponent<Rigidbody2D>();
+                obstacleRigidbody.velocity = Vector2.left * obstacleSpeedMultiplier;
+            }
+        }
+    }
+}
