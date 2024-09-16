59cd15efa8f0b63a0e63146f7a5b43b5c38d242a
dotmidi <0994510@hr.nl>
2024-09-16 22:04:57 +0200

    First version popup menus
    Leaderboard implemented, will do rest after approval from designer

--- TabbyCake-ShapeShifter/Assets/Scripts/SceneLoader.cs
+++ TabbyCake-ShapeShifter/Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;  // Needed to manage scene loading
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject LeaderboardCanvas;
+    public GameObject CosmeticsCanvas;
+    public GameObject SettingsCanvas;
+    public GameObject CreditsCanvas;
     void Start()
     {
         Application.targetFrameRate = 60;  // Set the target frame rate to 60
@@ -13,5 +17,25 @@ public class SceneLoader : MonoBehaviour
     public void NextScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void PopUp(string menuName)
+    {
+        // make a switch case with options leaderboard, cosmetics, settings credits. These will uncover the canvas with the respective menu
+        switch (menuName)
+        {
+            case "leaderboard":
+                LeaderboardCanvas.SetActive(!LeaderboardCanvas.activeSelf);
+                break;
+            case "cosmetics":
+                CosmeticsCanvas.SetActive(!CosmeticsCanvas.activeSelf);
+                break;
+            case "settings":
+                SettingsCanvas.SetActive(!SettingsCanvas.activeSelf);
+                break;
+            case "credits":
+                CreditsCanvas.SetActive(!CreditsCanvas.activeSelf);
+                break;
+        }
     }
 }
