7c662b1c7ea8c4f51f29472e0cd6204b9e69ef20
DreeseR <roandries@hotmail.com>
2024-10-07 14:15:37 +0200

    cosmetics code all working, just a couple things left before it is done completely

--- TabbyCake-ShapeShifter/Assets/Scripts/UIFunctions.cs
+++ TabbyCake-ShapeShifter/Assets/Scripts/UIFunctions.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using JetBrains.Annotations;
 
 public class UIFunctions : MonoBehaviour
 {
@@ -37,25 +38,25 @@ public class UIFunctions : MonoBehaviour
 
     [Header("Cosmetics")]
     public CosmeticSaveDataModel cosmeticSaveData;
-    public Image squareSelect;
+    public Image squareSelector;
     public Button square0;
     public Button square1;
     public Button square2;
     public Button square3;
     public Button square4;
-    public Image triangleSelect;
+    public Image triangleSelector;
     public Button triangle0;
     public Button triangle1;
     public Button triangle2;
     public Button triangle3;
     public Button triangle4;
-    public Image circleSelect;
+    public Image circleSelector;
     public Button circle0;
     public Button circle1;
     public Button circle2;
     public Button circle3;
     public Button circle4;
-    public Image diamondSelect;
+    public Image diamondSelector;
     public Button diamond0;
     public Button diamond1;
     public Button diamond2;
@@ -284,31 +285,34 @@ public class UIFunctions : MonoBehaviour
 
     void CosmeticsInit()
     {
+        //initialise save data object
         cosmeticSaveData = new CosmeticSaveDataModel();
-        //square buttons
+        //move selectors to correct position
+        SetSelectorPositions();
+        //assign square buttons
         square0.onClick.AddListener(delegate{ModifyCosmeticData(Shape.SQUARE, 0);});
         square1.onClick.AddListener(delegate{ModifyCosmeticData(Shape.SQUARE, 1);});
         square2.onClick.AddListener(delegate{ModifyCosmeticData(Shape.SQUARE, 2);});
         square3.onClick.AddListener(delegate{ModifyCosmeticData(Shape.SQUARE, 3);});
         square4.onClick.AddListener(delegate{ModifyCosmeticData(Shape.SQUARE, 4);});
-        //triangle buttons
+        //assign triangle buttons
         triangle0.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 0);});
         triangle1.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 1);});
         triangle2.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 2);});
         triangle3.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 3);});
         triangle4.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 4);});
-        //circle buttons
+        //assign circle buttons
         circle0.onClick.AddListener(delegate{ModifyCosmeticData(Shape.CIRCLE, 0);});
         circle1.onClick.AddListener(delegate{ModifyCosmeticData(Shape.CIRCLE, 1);});
         circle2.onClick.AddListener(delegate{ModifyCosmeticData(Shape.CIRCLE, 2);});
         circle3.onClick.AddListener(delegate{ModifyCosmeticData(Shape.CIRCLE, 3);});
         circle4.onClick.AddListener(delegate{ModifyCosmeticData(Shape.CIRCLE, 4);});
-        //diamond buttons
-        triangle0.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 0);});
-        triangle1.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 1);});
-        triangle2.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 2);});
-        triangle3.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 3);});
-        triangle4.onClick.AddListener(delegate{ModifyCosmeticData(Shape.TRIANGLE, 4);});
+        //assign diamond buttons
+        diamond0.onClick.AddListener(delegate{ModifyCosmeticData(Shape.DIAMOND, 0);});
+        diamond1.onClick.AddListener(delegate{ModifyCosmeticData(Shape.DIAMOND, 1);});
+        diamond2.onClick.AddListener(delegate{ModifyCosmeticData(Shape.DIAMOND, 2);});
+        diamond3.onClick.AddListener(delegate{ModifyCosmeticData(Shape.DIAMOND, 3);});
+        diamond4.onClick.AddListener(delegate{ModifyCosmeticData(Shape.DIAMOND, 4);});
     }
 
     public void ModifyCosmeticData(Shape shape, int SpriteID)
@@ -317,18 +321,42 @@ public class UIFunctions : MonoBehaviour
         if (shape == Shape.SQUARE)
         {
             cosmeticSaveData.squareSprite = SpriteID;
+            if (SpriteID == 0) { squareSelector.rectTransform.position = square0.transform.position; }
+            else if (SpriteID == 1) { squareSelector.rectTransform.position = square1.transform.position; }
+            else if (SpriteID == 2) { squareSelector.rectTransform.position = square2.transform.position; }
+            else if (SpriteID == 3) { squareSelector.rectTransform.position = square3.transform.position; }
+            else if (SpriteID == 4) { squareSelector.rectTransform.position = square4.transform.position; }
+            //cosmeticSaveData.squareSelectPos = squareSelector.rectTransform.position;
         }
         if (shape == Shape.TRIANGLE)
         {
             cosmeticSaveData.triangleSprite = SpriteID;
+            if (SpriteID == 0) { triangleSelector.rectTransform.position = triangle0.transform.position; }
+            else if (SpriteID == 1) { triangleSelector.rectTransform.position = triangle1.transform.position; }
+            else if (SpriteID == 2) { triangleSelector.rectTransform.position = triangle2.transform.position; }
+            else if (SpriteID == 3) { triangleSelector.rectTransform.position = triangle3.transform.position; }
+            else if (SpriteID == 4) { triangleSelector.rectTransform.position = triangle4.transform.position; }
+            //cosmeticSaveData.triangleSelectPos = triangleSelector.rectTransform.position;
         }
         if (shape == Shape.CIRCLE)
         {
             cosmeticSaveData.circleSprite = SpriteID;
+            if (SpriteID == 0) { circleSelector.rectTransform.position = circle0.transform.position; }
+            else if(SpriteID == 1) { circleSelector.rectTransform.position = circle1.transform.position; }
+            else if (SpriteID == 2) { circleSelector.rectTransform.position = circle2.transform.position; }
+            else if (SpriteID == 3) { circleSelector.rectTransform.position = circle3.transform.position; }
+            else if (SpriteID == 4) { circleSelector.rectTransform.position = circle4.transform.position; }
+            //cosmeticSaveData.circleSelectPos = circleSelector.rectTransform.position;
         }
         if (shape == Shape.DIAMOND)
         {
             cosmeticSaveData.diamondSprite = SpriteID;
+            if (SpriteID == 0) { diamondSelector.rectTransform.position = diamond0.transform.position; }
+            else if (SpriteID == 1) { diamondSelector.rectTransform.position = diamond1.transform.position; }
+            else if (SpriteID == 2) { diamondSelector.rectTransform.position = diamond2.transform.position; }
+            else if (SpriteID == 3) { diamondSelector.rectTransform.position = diamond3.transform.position; }
+            else if (SpriteID == 4) { diamondSelector.rectTransform.position = diamond4.transform.position; }
+            //cosmeticSaveData.diamondSelectPos = diamondSelector.rectTransform.position;
         }
         SaveCosmeticData();
     }
@@ -355,10 +383,10 @@ public class UIFunctions : MonoBehaviour
             Debug.Log("Triangle sprite ID:" + cosmeticSaveData.triangleSprite);
             Debug.Log("Circle sprite ID:" + cosmeticSaveData.circleSprite);
             Debug.Log("Diamond sprite ID:" + cosmeticSaveData.diamondSprite);
-            Debug.Log("Square select Position:" + cosmeticSaveData.squareSelectPos);
-            Debug.Log("Triangle select Position:" + cosmeticSaveData.triangleSelectPos);
-            Debug.Log("Circle select Position:" + cosmeticSaveData.circleSelectPos);
-            Debug.Log("Diamond select Position:" + cosmeticSaveData.diamondSelectPos);
+            //Debug.Log("Square select Position:" + cosmeticSaveData.squareSelectPos);
+            //Debug.Log("Triangle select Position:" + cosmeticSaveData.triangleSelectPos);
+            //Debug.Log("Circle select Position:" + cosmeticSaveData.circleSelectPos);
+            //Debug.Log("Diamond select Position:" + cosmeticSaveData.diamondSelectPos);
         }
         else
         {
@@ -368,7 +396,41 @@ public class UIFunctions : MonoBehaviour
             SaveCosmeticData();
         }
     }
+    public void SetSelectorPositions()
+    {
+        LoadCosmeticData();
+        //square selector
+        if (cosmeticSaveData.squareSprite == 0) { squareSelector.rectTransform.position = square0.transform.position; }
+        else if (cosmeticSaveData.squareSprite == 1) { squareSelector.rectTransform.position = square1.transform.position; }
+        else if (cosmeticSaveData.squareSprite == 2) { squareSelector.rectTransform.position = square2.transform.position; }
+        else if (cosmeticSaveData.squareSprite == 3) { squareSelector.rectTransform.position = square3.transform.position; }
+        else if (cosmeticSaveData.squareSprite == 4) { squareSelector.rectTransform.position = square4.transform.position; }
+        //triangle selector
+        if (cosmeticSaveData.triangleSprite == 0) { triangleSelector.rectTransform.position = triangle0.transform.position; }
+        else if (cosmeticSaveData.triangleSprite == 1) { triangleSelector.rectTransform.position = triangle1.transform.position; }
+        else if (cosmeticSaveData.triangleSprite == 2) { triangleSelector.rectTransform.position = triangle2.transform.position; }
+        else if (cosmeticSaveData.triangleSprite == 3) { triangleSelector.rectTransform.position = triangle3.transform.position; }
+        else if (cosmeticSaveData.triangleSprite == 4) { triangleSelector.rectTransform.position = triangle4.transform.position; }
+        //circle selector
+        if (cosmeticSaveData.circleSprite == 0) { circleSelector.rectTransform.position = circle0.transform.position; }
+        else if (cosmeticSaveData.circleSprite == 1) { circleSelector.rectTransform.position = circle1.transform.position; }
+        else if (cosmeticSaveData.circleSprite == 2) { circleSelector.rectTransform.position = circle2.transform.position; }
+        else if (cosmeticSaveData.circleSprite == 3) { circleSelector.rectTransform.position = circle3.transform.position; }
+        else if (cosmeticSaveData.circleSprite == 4) { circleSelector.rectTransform.position = circle4.transform.position; }
+        //diamond selector
+        if (cosmeticSaveData.diamondSprite == 0) { diamondSelector.rectTransform.position = diamond0.transform.position; }
+        else if (cosmeticSaveData.diamondSprite == 1) { diamondSelector.rectTransform.position = diamond1.transform.position; }
+        else if (cosmeticSaveData.diamondSprite == 2) { diamondSelector.rectTransform.position = diamond2.transform.position; }
+        else if (cosmeticSaveData.diamondSprite == 3) { diamondSelector.rectTransform.position = diamond3.transform.position; }
+        else if (cosmeticSaveData.diamondSprite == 4) { diamondSelector.rectTransform.position = diamond4.transform.position; }
 
+        //squareSelector.rectTransform.position = cosmeticSaveData.squareSelectPos;
+        //triangleSelector.rectTransform.position = cosmeticSaveData.triangleSelectPos;
+        //circleSelector.rectTransform.position = cosmeticSaveData.circleSelectPos;
+        //diamondSelector.rectTransform.position = cosmeticSaveData.diamondSelectPos;
+    }
+
+    /*
     void ProcessSaveData(CosmeticSaveDataModel cosmeticSaveData)
     {
         int square = cosmeticSaveData.squareSprite;
@@ -376,6 +438,7 @@ public class UIFunctions : MonoBehaviour
         int circle = cosmeticSaveData.circleSprite;
         int diamond = cosmeticSaveData.diamondSprite;
     }
+    */
 }
 
 [Serializable]
@@ -385,8 +448,8 @@ public class CosmeticSaveDataModel
     public int triangleSprite;
     public int circleSprite;
     public int diamondSprite;
-    public Vector3 squareSelectPos;
-    public Vector3 triangleSelectPos;
-    public Vector3 circleSelectPos;
-    public Vector3 diamondSelectPos;
+    //public Vector3 squareSelectPos;
+    //public Vector3 triangleSelectPos;
+    //public Vector3 circleSelectPos;
+    //public Vector3 diamondSelectPos;
 }
